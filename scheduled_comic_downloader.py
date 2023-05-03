d4d3fb52e5641c0308b79d1b6d19d5bafbb8265f
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-03 12:23:12 -0500

    Finish Pizza Cake download logic.

--- scheduled_comic_downloader.py
+++ scheduled_comic_downloader.py
@@ -3,6 +3,7 @@
 # For more information, see project_details.txt.
 
 import logging
+import os
 import requests
 import bs4
 
@@ -23,23 +24,40 @@ def get_pizza_cake():
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "lxml")
     for link in soup.find_all("img"):
-        image = link.get("src")
-        if image.endswith(".png"):
+        image_link = link.get("src")
+        if image_link.endswith(".png"):
             pizza_cake.append(link.get("src"))
+    os.makedirs("./pizza_cake", exist_ok=True)
     return pizza_cake
 
 
-def download_comic_thread(comic):
+def download_comic(comic):
     res = requests.get(comic)
     res.raise_for_status()
-    comic_name = comic.split("/")
-    print(f"Downloading {comic_name[-1]} ...")
-    with open(f"{comic_name[-1]}.png", "wb") as f:
+    comic_name = comic.split("/")[-1]
+    print(f"Downloading {comic_name} ...")
+    with open(f"./pizza_cake/{comic.split('/')[-1]}", "wb") as f:
         for chunk in res.iter_content(100_000):
             f.write(chunk)
 
 
-pizza_list = get_pizza_cake()
+def check_pizza_cake():
+    pizza_list = get_pizza_cake()
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(download_comic_thread, pizza_list)
+    existing_files = os.listdir("./pizza_cake")
+
+    missing_pizza_cake = [
+        file_name
+        for file_name in pizza_list
+        if file_name.split("/")[-1] not in existing_files
+    ]
+
+    if len(missing_pizza_cake) > 10:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(download_comic, missing_pizza_cake)
+    else:
+        for comic in missing_pizza_cake:
+            download_comic(comic)
+
+
+check_pizza_cake()
