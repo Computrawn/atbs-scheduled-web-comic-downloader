a56bdc3745b2a9c90d616b8973657e0179d44754
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-05-04 14:52:41 -0500

    Replace code on main.

--- scheduled_comic_downloader.py
+++ scheduled_comic_downloader.py
@@ -2,65 +2,77 @@
 # scheduled_comic_downloader.py — An exercise in keeping time and scheduling.
 # For more information, see project_details.txt.
 
+#! python3
+# comicdldr_class.py — First try at OOP.
+
 import logging
 import os
-from concurrent.futures import ThreadPoolExecutor
 import requests
 import bs4
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename="logging.txt",
+    filename="class_logging.txt",
     format="%(asctime)s -  %(levelname)s -  %(message)s",
 )
-# logging.disable(logging.CRITICAL)  # Note out to enable logging.
-
-web_comics_urls = [
-    "https://pizzacakecomic.com",
-    # "https://www.savagechickens.com",
-    # "https://www.buttersafe.com",
-]
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def get_pizza_cake():
-    pizza_cake = []
-    res = requests.get(f"{web_comics_urls[0]}")
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, "lxml")
-    # comic_name = web_comics_urls[0].split("//")[-1][:-4]
-    for link in soup.find_all("img"):
-        image_link = link.get("src")
-        if image_link.endswith((".png", ".jpg")):
-            pizza_cake.append(link.get("src"))
-    os.makedirs(f"./pizza_cake", exist_ok=True)
-    return pizza_cake
+class Comic:
+    def __init__(self, name, website):
+        self.name = name
+        self.website = website
+
+    def get_comic(self):
+        comic_list = []
+        res = requests.get(self.website)
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, "lxml")
+
+        for link in soup.find_all("img"):
+            image_link = link.get("src")
+
+            if image_link.endswith((".png", ".jpg", ".gif")):
+                comic_list.append(link.get("src"))
+
+        os.makedirs(f"./{self.name}", exist_ok=True)
+
+        existing_files = os.listdir(f"./{self.name}")
+        ignore_files = [
+            "callout-store3.jpg",
+            "ebookad410_fear.gif",
+            "footerlogo.gif",
+            "icon_chickens100.jpg",
+            "icon_computer.jpg",
+            "icon_random.gif",
+            "LMRB4hero250.jpg",
+            "mug.jpg",
+            "Savage_Chickens_Logo.jpg",
+            "wfacebook.gif",
+            "wtvtropes.gif",
+            "wtwitter.gif",
+        ]
+        missing_files = [
+            file_name
+            for file_name in comic_list
+            if file_name.split("/")[-1] not in ignore_files
+            if file_name.split("/")[-1] not in existing_files
+        ]
+
+        for comic in missing_files:
+            comic_res = requests.get(comic)
+            comic_res.raise_for_status()
+            comic_name = comic.split("/")[-1]
+            print(f"Downloading {comic_name} ...")
+            with open(f"./{self.name}/{comic_name}", "wb") as f:
+                for chunk in comic_res.iter_content(100_000):
+                    f.write(chunk)
 
 
-def download_comic(comic):
-    res = requests.get(comic)
-    res.raise_for_status()
-    comic_name = comic.split("/")[-1]
-    print(f"Downloading {comic_name} ...")
-    with open(f"./pizza_cake/{comic.split('/')[-1]}", "wb") as f:
-        for chunk in res.iter_content(100_000):
-            f.write(chunk)
+comic_1 = Comic("pizza_cake", "https://pizzacakecomic.com")
+comic_2 = Comic("savage_chickens", "https://www.savagechickens.com")
+comic_3 = Comic("wonderella", "https://nonadventures.com")
 
-
-def check_pizza_cake():
-    pizza_list = get_pizza_cake()
-    existing_files = os.listdir("./pizza_cake")
-    missing_pizza_cake = [
-        file_name
-        for file_name in pizza_list
-        if file_name.split("/")[-1] not in existing_files
-    ]
-
-    if len(missing_pizza_cake) > 10:
-        with ThreadPoolExecutor() as executor:
-            executor.map(download_comic, missing_pizza_cake)
-    else:
-        for comic in missing_pizza_cake:
-            download_comic(comic)
-
-
-check_pizza_cake()
+comic_1.get_comic()
+comic_2.get_comic()
+comic_3.get_comic()
